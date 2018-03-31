10f1b184be6163b9b6e25b32333cbab209439fd2
Naresh Indukuri <kalpanacj@gmail.com>
2018-03-31 15:08:49 -0500

    caesar.py

--- caesar.py
+++ caesar.py
@@ -1,15 +1,39 @@
-from helpers import alphabet_position, rotate_character
+def alphabet_position(character):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    lower = character.lower()
+    return alphabet.index(lower)
 
-def encrypt(text, rot):
-    init = ""
-    for letter in text:
-        init += rotate_character(letter, rot)
-    return init
+def rotate_string_13(text):
 
-def main():
-    message = input("Type a message:\n")
-    rotation = int(input("Rotate by:\n"))
-    print(encrypt(message,rotation))
+    rotated = ''
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-if __name__ == "__main__":
-    main()                 
+    for char in text:
+        rotated_idx = (alphabet_position(char) + 13) % 26
+        if char.isupper():
+            rotated = rotated + alphabet[rotated_idx].upper()
+        else:
+            rotated = rotated + alphabet[rotated_idx]
+
+    return rotated
+
+def rotate_character(char, rot):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    rotated_idx = (alphabet_position(char) + rot) % 26
+
+    if char.isupper():
+        return alphabet[rotated_idx].upper()
+    else:
+        return alphabet[rotated_idx]
+
+def rotate_string(text, rot):
+
+    rotated = ''
+
+    for char in text:
+        if (char.isalpha()):
+            rotated = rotated + rotate_character(char, rot)
+        else:
+            rotated = rotated + char
+
+    return rotated
